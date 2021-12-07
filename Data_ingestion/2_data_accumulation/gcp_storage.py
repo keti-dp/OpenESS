02667ed08fc8029a113a5f39ba8524875933af07
윤태일 <31847211+YunTaeIl@users.noreply.github.com>
2021-12-07 13:29:05 +0900

    Update gcp_storage.py

--- Data_ingestion/2_data_accumulation/gcp_storage.py
+++ Data_ingestion/2_data_accumulation/gcp_storage.py
@@ -1,7 +1,10 @@
 """
-Copyright 2021, KETI.
+gcp_storage.py : GCS(Google Cloud Storage) 파일 업로드 모듈
 
-2021-12-06 ver 1.0 gcp_storage.py 
+Copyright(C) 2021, 윤태일 / KETI / taeil777@keti.re.kr
+
+최신 테스트 버전 : 1.0 ver
+최신 안정화 버전 : 1.0 ver
 
 GCS에 만들어진 bucket에 파일을 업로드하는 모듈 코드입니다.
 
