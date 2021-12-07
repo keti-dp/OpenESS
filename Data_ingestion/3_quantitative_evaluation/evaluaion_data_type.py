6f0506537c55fb6a4201912084e174ae01c9bb9b
윤태일 <31847211+YunTaeIl@users.noreply.github.com>
2021-12-07 13:29:13 +0900

    Update evaluaion_data_type.py

--- Data_ingestion/3_quantitative_evaluation/evaluaion_data_type.py
+++ Data_ingestion/3_quantitative_evaluation/evaluaion_data_type.py
@@ -1,9 +1,12 @@
 # -*- coding:utf-8 -*-
 
 """
-Copyright 2021, KETI.
+evaluaion_data_type.py : 대규모 분산 에너지 저장장치 과제 정량평가 테스트 코드
 
-2021-12-06 ver 1.0 evaluaion_data_type.py 
+Copyright(C) 2021, 윤태일 / KETI / taeil777@keti.re.kr
+
+최신 테스트 버전 : 1.0 ver
+최신 안정화 버전 : 1.0 ver
 
 ESS 정량평가 중 수집 데이터 종류 평가를 위한 테스트 코드입니다. 
 
