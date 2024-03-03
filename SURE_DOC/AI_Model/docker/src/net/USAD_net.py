1c2eee616918e86336dd3cf0e295074935c154fb
JeongHun Woo <wjn1208@icloud.com>
2024-03-04 08:21:22 +0900

    Docker_Packaging_Upload

---
+++ SURE_DOC/AI_Model/docker/src/net/USAD_net.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+class Encoder(nn.Module):
+  def __init__(self, in_size, latent_size):
+    super().__init__()
+    self.linear1 = nn.Linear(in_size, int(in_size/2))
+    self.linear2 = nn.Linear(int(in_size/2), int(in_size/4))
+    self.linear3 = nn.Linear(int(in_size/4), latent_size)
+    self.relu = nn.ReLU(True)
+        
+  def forward(self, w):
+    out = self.linear1(w)
+    out = self.relu(out)
+    out = self.linear2(out)
+    out = self.relu(out)
+    out = self.linear3(out)
+    z = self.relu(out)
+    return z
+    
+class Decoder(nn.Module):
+  def __init__(self, latent_size, out_size):
+    super().__init__()
+    self.linear1 = nn.Linear(latent_size, int(out_size/4))
+    self.linear2 = nn.Linear(int(out_size/4), int(out_size/2))
+    self.linear3 = nn.Linear(int(out_size/2), out_size)
+    self.relu = nn.ReLU(True)
+    self.sigmoid = nn.Sigmoid()
+        
+  def forward(self, z):
+    out = self.linear1(z)
+    out = self.relu(out)
+    out = self.linear2(out)
+    out = self.relu(out)
+    out = self.linear3(out)
+    w = self.sigmoid(out)
+    return w
+    
---
+++ SURE_DOC/AI_Model/docker/src/net/__init__.py
@@ -0,0 +1,2 @@
+from .USAD_net import Encoder, Decoder
+from .DeepAnt_net import DeepAntNet
