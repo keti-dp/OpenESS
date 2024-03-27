3c61aa25bd7e392dcb6c4bf5a681eb0985901e68
wjn1208 <wjn1208@icloud.com>
2024-03-27 15:12:59 +0900

    dataset_path 수정

--- SURESOFT/AI/Docker/src/deepant_train.sh
+++ SURESOFT/AI/Docker/src/deepant_train.sh
@@ -1,1 +1,1 @@
-python main.py --dataset "/STORAGE/ESS/01_DATA/TESTBED/0731_data_sc3_v1.csv" --mode "train" --model "deepant" --lr 1e-3 --n_epochs 150 --batch_size 2048 --features "CPU usage" --es_epochs 10
+python main.py --dataset "./data/train.csv" --mode "train" --model "deepant" --lr 1e-3 --n_epochs 150 --batch_size 2048 --features "CPU usage" --es_epochs 10
