782c5a014416605fc9610ec47487a5981e2c2e8f
wjn1208 <wjn1208@icloud.com>
2024-11-20 11:23:30 +0900

    lstm_usad sh file modify

--- SURESOFT/AI/Docker/src/lstm_usad_train.sh
+++ SURESOFT/AI/Docker/src/lstm_usad_train.sh
@@ -1,6 +1,6 @@
 #Train
-python main.py --dataset './data/train.csv' --mode "train" --model "lstm_usad" --lr 1e-3 --n_epochs 300 --batch_size 2048 --features "CPU usage" --es_epochs 300
+python main.py --dataset './data/train.csv' --mode "train" --model "lstm_usad" --lr 1e-3 --n_epochs 300 --batch_size 2048 --features "CPU usage" "Heap current" "Time stamp" --es_epochs 300
 
 
 #Predict
-python main.py --dataset "./data/test.csv" --mode "pred" --model "lstm_usad" --lr 1e-4 --n_epochs 150 --batch_size 1 --features "CPU usage" --es_epochs 15
+python main.py --dataset "./data/test.csv" --mode "pred" --model "lstm_usad" --lr 1e-4 --n_epochs 150 --batch_size 1 --features "CPU usage" "Heap current" "Time stamp" --es_epochs 15
