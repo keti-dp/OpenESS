376772f8f45e4682f2f0e20333c06a8bb5995c0f
Dongcheol_Lee <135970051+ESCL9883@users.noreply.github.com>
2024-12-03 17:58:38 +0900

    Update ESS_testbed_RLS.py

--- CNU/Fault diagnosis/ESS_testbed_RLS.py
+++ CNU/Fault diagnosis/ESS_testbed_RLS.py
@@ -16,12 +16,12 @@
 # Ri, Rdiff : S/P
 # Cdiff : P/S
 
-rack_data = pd.read_csv('경로 선정 필요')
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
 
-# Bank, Rack ID 선정
-rack_voltage = rack_data[(rack_data['BANK_ID'] == 1) & (rack_data['RACK_ID'] == 4)]['RACK_VOLTAGE'].values
-rack_current = rack_data[(rack_data['BANK_ID'] == 1) & (rack_data['RACK_ID'] == 4)]['RACK_CURRENT'].values
-rack_soc = rack_data[(rack_data['BANK_ID'] == 1) & (rack_data['RACK_ID'] == 4)]['RACK_SOC'].values
 
 SOC = np.linspace(1, 0, num=21)
 
@@ -29,11 +29,25 @@ OCV_PL2 = loadmat('경로 선정 필요')
 
 OCV_ESS = OCV_PL2
 
-Time = 1
-Init_cap = 228.57
-Init_Ri = 0.0310707902382320 * 4
-Init_Rdiff = 0.0190371443335961 * 4
-Init_Cdiff = 6093.350870660123 / 4
+SOC = np.linspace(1, 0, num=21)
+
+# Create SOC_OCV and OCV_SOC lookup tables
+SOC_OCV_lookup = dict(zip(SOC, OCV_ESS))
+OCV_SOC_lookup = dict(zip(OCV_ESS, SOC))
+
+rack_data = pd.read_csv('경로 선정 필요')
+
+# 비교 분석할 Bank, Rack ID 선정
+rack_voltage_1 = rack_data[(rack_data['BANK_ID'] == 1) & (rack_data['RACK_ID'] == 1)]['RACK_VOLTAGE'].values
+rack_voltage_4 = rack_data[(rack_data['BANK_ID'] == 1) & (rack_data['RACK_ID'] == 4)]['RACK_VOLTAGE'].values
+rack_current_1 = rack_data[(rack_data['BANK_ID'] == 1) & (rack_data['RACK_ID'] == 1)]['RACK_CURRENT'].values
+rack_current_4 = rack_data[(rack_data['BANK_ID'] == 1) & (rack_data['RACK_ID'] == 4)]['RACK_CURRENT'].values
+
+# Define initial parameters
+Init_cap = 228.57  # Ah
+Init_Ri = 0.0310707902382320 * 4  # Initial internal resistance (adjusted for rack size)
+Init_Rdiff = 0.0190371443335961 * 4  # Initial Rdiff
+Init_Cdiff = 6093.350870660123 / 4  # Initial Cdiff
 ErrorCovariance = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 ForgettingFactor = 0.99999999
 
@@ -41,15 +55,9 @@ Vector_b0 = Init_Ri
 Vector_b1 = (-Init_Ri + 1 / Init_Cdiff + Init_Ri / (Init_Rdiff * Init_Cdiff))
 Vector_a1 = (1 / (Init_Rdiff * Init_Cdiff) - 1)
 
-SOC_OCV_lookup = dict(zip(SOC, OCV_ESS))
-OCV_SOC_lookup = dict(zip(OCV_ESS, SOC))
-
-# SOC_OCV_lookup 테이블을 역으로 변환하여 SOC에 따른 OCV를 찾는 Lookup table 생성
-sorted_SOC_OCV_lookup = sorted(SOC_OCV_lookup.items())
-sorted_OCV_SOC_lookup = sorted(OCV_SOC_lookup.items())
-
+# SOC lookup functions
 def find_soc_from_voltage(voltage, lookup_table):
-    ocvs, socs = zip(*lookup_table)  
+    ocvs, socs = zip(*lookup_table.items())  # Get items from dictionary
     ocvs = np.array(ocvs)
     socs = np.array(socs)
 
@@ -65,7 +73,7 @@ def find_soc_from_voltage(voltage, lookup_table):
     return extrapolated_soc
 
 def find_ocv_from_soc(soc, lookup_table):
-    socs, ocvs = zip(*lookup_table)  
+    socs, ocvs = zip(*lookup_table.items())  # Get items from dictionary
     socs = np.array(socs)
     ocvs = np.array(ocvs)
 
@@ -80,30 +88,32 @@ def find_ocv_from_soc(soc, lookup_table):
     
     return extrapolated_ocv
 
+# Initial SOC calculation from voltage for Rack 1 and Rack 4
+Init_soc_rack_1 = [find_soc_from_voltage(vol, SOC_OCV_lookup) for vol in rack_voltage_1]
+Init_soc_rack_4 = [find_soc_from_voltage(vol, SOC_OCV_lookup) for vol in rack_voltage_4]
 
+# SOC Ref for Rack 1 and Rack 4 (calculating SOC over time)
+SOC_ref_rack_1 = []
+current_SOC_1 = Init_soc_rack_1[0]
 
+for i in range(len(rack_current_1)):
+    delta_SOC = (rack_current_1[i] / Init_cap) * (1 / 3600)
+    current_SOC_1 += delta_SOC
+    SOC_ref_rack_1.append(current_SOC_1)
 
-# Initial SOC calculation from voltage
-Init_soc_rack = [find_soc_from_voltage(vol, sorted_OCV_SOC_lookup) for vol in rack_voltage]
+SOC_ref_rack_4 = []
+current_SOC_4 = Init_soc_rack_4[0]
 
-# Ah counting to calculate SOC
-SOC_ref_rack = []
-current_SOC = Init_soc_rack[0]
+for i in range(len(rack_current_4)):
+    delta_SOC = (rack_current_4[i] / Init_cap) * (1 / 3600)
+    current_SOC_4 += delta_SOC
+    SOC_ref_rack_4.append(current_SOC_4)
 
-for i in range(len(rack_current)):
-    delta_SOC = (rack_current[i] / Init_cap) * (Time / 3600)
-    current_SOC += delta_SOC
-    SOC_ref_rack.append(current_SOC)
+# OCV for both racks
+OCV_rack_1 = [find_ocv_from_soc(soc, SOC_OCV_lookup) for soc in SOC_ref_rack_1]
+OCV_rack_4 = [find_ocv_from_soc(soc, SOC_OCV_lookup) for soc in SOC_ref_rack_4]
 
-OCV_rack = [find_ocv_from_soc(soc, sorted_SOC_OCV_lookup) for soc in SOC_ref_rack]
-
-theta = np.array([Vector_b0, Vector_b1, Vector_a1]).reshape(-1, 1)
-P = ErrorCovariance
-
-Ri_estimates = [Init_Ri]
-Rdiff_estimates = [Init_Rdiff]
-Cdiff_estimates = [Init_Cdiff]
-
+# RLS method for estimation of Ri, Rdiff, Cdiff
 def calculate_gain_and_covariance(phi, P, forgetting_factor):
     P_phi = P @ phi
     gain_denominator = forgetting_factor + phi.T @ P @ phi 
@@ -112,56 +122,70 @@ def calculate_gain_and_covariance(phi, P, forgetting_factor):
     covariance = np.clip(covariance, 0, 1e-10)  # Clip the values of covariance
     return gain, covariance
 
-gain_values = []
-overpotential_values = []
-RlsEstimatedVoltage_values = []
-RlsVoltageError_values = []
-covariance_values = []
-theta_values = []
-phi_values = []
+# Estimation for Rack 1 and Rack 4
+Ri_estimates_1, Rdiff_estimates_1, Cdiff_estimates_1 = [Init_Ri], [Init_Rdiff], [Init_Cdiff]
+Ri_estimates_4, Rdiff_estimates_4, Cdiff_estimates_4 = [Init_Ri], [Init_Rdiff], [Init_Cdiff]
 
-for t in range(1, len(rack_voltage)):
-    voltage = rack_voltage[t]
-    current = rack_current[t]
-    prev_current = rack_current[t-1]
-    ocv = OCV_rack[t]
+for t in range(1, len(rack_voltage_1)):
+    voltage_1 = rack_voltage_1[t]
+    current_1 = rack_current_1[t]
+    prev_current_1 = rack_current_1[t-1]
+    ocv_1 = OCV_rack_1[t]
+    
+    voltage_4 = rack_voltage_4[t]
+    current_4 = rack_current_4[t]
+    prev_current_4 = rack_current_4[t-1]
+    ocv_4 = OCV_rack_4[t]
 
-    overpotential = ocv - voltage
-    overpotential_values.append(overpotential)
+    phi_1 = np.array([current_1, prev_current_1, ocv_1 - voltage_1]).reshape(-1, 1)
+    gain_1, covariance_1 = calculate_gain_and_covariance(phi_1, ErrorCovariance, ForgettingFactor)
+    ErrorCovariance = covariance_1
+    Ri_1 = np.abs(gain_1[0, 0])
+    Rdiff_1 = np.abs(gain_1[1, 0] - gain_1[2, 0] * gain_1[0, 0]) / (1 - gain_1[2, 0])
+    Cdiff_1 = 1 / (gain_1[1, 0] - gain_1[2, 0] * gain_1[0, 0])
 
-    phi = np.array([current, prev_current, overpotential]).reshape(-1, 1)
-    phi_values.append(phi.flatten())
+    Ri_estimates_1.append(Ri_1)
+    Rdiff_estimates_1.append(Rdiff_1)
+    Cdiff_estimates_1.append(Cdiff_1)
 
-    RlsEstimatedVoltage = theta.T @ phi
-    RlsEstimatedVoltage_values.append(RlsEstimatedVoltage[0, 0])
+    phi_4 = np.array([current_4, prev_current_4, ocv_4 - voltage_4]).reshape(-1, 1)
+    gain_4, covariance_4 = calculate_gain_and_covariance(phi_4, ErrorCovariance, ForgettingFactor)
+    ErrorCovariance = covariance_4
+    Ri_4 = np.abs(gain_4[0, 0])
+    Rdiff_4 = np.abs(gain_4[1, 0] - gain_4[2, 0] * gain_4[0, 0]) / (1 - gain_4[2, 0])
+    Cdiff_4 = 1 / (gain_4[1, 0] - gain_4[2, 0] * gain_4[0, 0])
 
-    RlsVoltageError = voltage - ocv - RlsEstimatedVoltage
-    RlsVoltageError_values.append(RlsVoltageError[0, 0])
+    Ri_estimates_4.append(Ri_4)
+    Rdiff_estimates_4.append(Rdiff_4)
+    Cdiff_estimates_4.append(Cdiff_4)
 
+# Calculate variations between Rack 1 and Rack 4
+Rdiff_variations = []
+Cdiff_variations = []
 
-    gain, covariance = calculate_gain_and_covariance(phi, P, ForgettingFactor)
-    gain_values.append(gain.flatten())
-    covariance_values.append(covariance.flatten())
+for rmax, rmin, cmax, cmin in zip(Rdiff_estimates_1, Rdiff_estimates_4, Cdiff_estimates_1, Cdiff_estimates_4):
+    Rdiff_variation = abs((rmax - rmin) / rmax) 
+    Cdiff_variation = abs((cmax - cmin) / cmax) 
+    Rdiff_variations.append(Rdiff_variation)
+    Cdiff_variations.append(Cdiff_variation)
 
-    P = covariance
+# Plot results (Rdiff and Cdiff variations)
+fig, ax1 = plt.subplots(figsize=(12, 6))
 
-    theta = theta + gain @ RlsVoltageError
-    theta = np.abs(theta)
-    theta_values.append(theta.flatten())
+ax1.plot(rack_voltage_1, label="Rack 1 Voltage")
+ax1.set_xlabel('Time Step')
+ax1.set_ylabel('Voltage (V)')
+ax1.set_ylim(800, 1000)
+ax1.set_xlim(1, len(rack_voltage_1))
 
-    Ri = theta[0, 0]
-    Rdiff = (theta[1, 0] - theta[2, 0] * theta[0, 0]) / (1 - theta[2, 0])
-    Cdiff = 1 / (theta[1, 0] - theta[2, 0] * theta[0, 0])
+# Plot Rdiff and Cdiff variations on the same plot with a secondary y-axis
+ax2 = ax1.twinx()
+ax2.plot(Rdiff_variations, color='orange', label='Rdiff Variation', linestyle='-')
+ax2.plot(Cdiff_variations, color='green', label='Cdiff Variation', linestyle='-')
+ax2.set_ylabel('Variation (%)')
 
-    Ri = np.abs(Ri)
-    Rdiff = np.abs(Rdiff)
-    Cdiff = np.abs(Cdiff)
-
-    Ri_estimates.append(Ri)
-    Rdiff_estimates.append(Rdiff)
-    Cdiff_estimates.append(Cdiff)
-
-gain_values = np.array(gain_values).reshape(-1, 3)
-covariance_values = np.array(covariance_values).reshape(-1, 9)  # 3x3 matrix flattened
-theta_values = np.array(theta_values).reshape(-1, 3)
-phi_values = np.array(phi_values).reshape(-1, 3)
+ax1.grid(True)
+ax1.legend(loc='upper left')
+ax2.legend(loc='upper right')
+plt.tight_layout()
+plt.show()
