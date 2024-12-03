6c6925b1f7324b3b88ddea51639bb90377c3928c
Dongcheol_Lee <135970051+ESCL9883@users.noreply.github.com>
2024-12-03 17:08:22 +0900

    Update RLS_dataset.py
    데이터 및 파일 경로 설정

--- CNU/Fault diagnosis/RLS_dataset.py
+++ CNU/Fault diagnosis/RLS_dataset.py
@@ -19,3 +19,22 @@ from keras.models import Model, load_model
 from keras.layers import Input, Dense
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras import regularizers
+
+
+
+# 배터리 정상/과충전/과방전 모듈(6S2P) 전압, 전류 데이터 셋 기반 내부 모델 파라미터(Ri, Rdiff, Cdiff) 추출
+
+# Load OCV data
+OCV = '경로 설정 필요'
+# 셀 level OCV는 OCV.csv 파일 사용
+
+SOC = np.linspace(1, 0, num=21)
+
+normal_dir = '경로 설정 필요'
+overcharge_dir = '경로 설정 필요'
+overdischarge_dir = '경로 설정 필요'
+save_dir = '경로 설정 필요'
+
+# 정상, 과충전 및 과방전 1~100사이클 충전 및 방전 구분해서 코드 실행 필요
+# ex) 첨부된 정상, 과충전, 과방전 팩 100사이클 충전/방전으로 dir 설정해서 코드 실행
+# 셀은 1~6열(셀 전압), 7열(모듈 전압), 8열(모듈 전류)
