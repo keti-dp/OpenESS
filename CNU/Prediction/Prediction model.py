7d6dcac9ebb78e2e78b754a422afd56927db1285
Dongcheol_Lee <135970051+ESCL9883@users.noreply.github.com>
2024-12-03 18:22:05 +0900

    Update Prediction model.py
    변수 생성

--- CNU/Prediction/Prediction model.py
+++ CNU/Prediction/Prediction model.py
@@ -16,3 +16,15 @@ hi_soh_data = hi_soh_data.to_numpy()
 print(hi_soh_data)
 hi_soh_data.shape
 
+#변수 생성
+hi_data = hi_soh_data[:,0:300]
+soh = hi_soh_data[:,-1]
+
+#나누기
+x_train = hi_data[:979, 1:] 
+y_train = soh[:979]         
+
+x_test = hi_data[979:, 1:]  
+y_test = soh[979:]  
+
+plt.plot(x_train)
